9a3465f5331b9d434ab9e4df8113d171842d7703
sam-zarila <zarilatroxx99@gmail.com>
2025-03-25 08:36:53 -0800

    making changes

--- src/components/Train.jsx
+++ src/components/Train.jsx
@@ -85,9 +85,9 @@ const trainCourses = [
 
 const Train = () => {
   return (
-    <section className="py-10 bg-gray-100">
+    <section className="py-10 bg-gray-800">
       <div className="max-w-7xl mx-auto px-6">
-        <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
+        <h2 className="text-3xl font-bold text-center text-gray-100 mb-6">
           Explore Our Training Courses
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
