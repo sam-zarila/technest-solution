2cd5327bdbf50fb8db0598023d9b7c9b98b98b68
crossby patson <efp-bsc-inf-03-20@unima.ac.mw>
2025-04-25 00:56:13 +0200

    addy

--- virtuals/adderall/adderallpayments.jsx
+++ virtuals/adderall/adderallpayments.jsx
@@ -93,8 +93,8 @@ const AdderalpayementPage = () => {
     const tx_ref = "TX-" + Date.now() + "-" + Math.floor(Math.random() * 1000);
 
     window.PaychanguCheckout({
-     // public_key: "PUB-TEST-PjxBxGsX32OVbBJbRJHFhwXwOOa9snAC",
-      public_key:"pub-live-Gdy7z1FBCG44EyGR2C58yWkpOWmRbdzt" ,
+      public_key: "PUB-TEST-PjxBxGsX32OVbBJbRJHFhwXwOOa9snAC",
+      //public_key:"pub-live-Gdy7z1FBCG44EyGR2C58yWkpOWmRbdzt" ,
       tx_ref,
       amount: formData.amount,
       currency: "MWK",
